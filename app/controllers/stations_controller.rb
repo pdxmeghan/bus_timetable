a5513f26063da18a95792e055c99ece34af80655
Meghan Lindsley & Tanner Steward <student@epicodus.com>
2014-09-02 12:10:20 -0700

    Add edit and update ability for Station model, refactor index page

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -22,6 +22,20 @@ class StationsController < ApplicationController
     @station =Station.find(params[:id])
   end
 
+  def edit
+    @station = Station.find(params[:id])
+  end
+
+  def update
+    @station = Station.find(params[:id])
+    if @station.update(user_params)
+      flash[:notice] = "Bus station updated"
+      redirect_to station_path(@station)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:station).permit(:name)
