4855899d804eb1790d250444b4c79f776110c991
Meghan Lindsley & Tanner Steward <student@epicodus.com>
2014-09-02 16:36:32 -0700

    made seven restful routes for stops

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -17,6 +17,31 @@ class StopsController < ApplicationController
     end
   end
 
+  def show
+    @stop = Stop.find(params[:id])
+  end
+
+  def edit
+    @stop = Stop.find(params[:id])
+  end
+
+  def update
+    @stop = Stop.find(params[:id])
+    if @stop.update(user_params)
+      flash[:notice] = "Stop has been updated"
+      redirect_to stop_path(@stop)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @stop = Stop.find(params[:id])
+    @stop.delete
+    flash[:notice] = "Stop Destroyed"
+    redirect_to stops_path
+  end
+
   private
   def user_params
     params.require(:stop).permit(:station_id, :line_id)
