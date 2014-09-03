6c7793952afeefcce5dc5af5943196881761d18d
Meghan Lindsley & Tanner Steward <student@epicodus.com>
2014-09-03 16:15:22 -0700

    Complete arrival model with full CRUD capability

--- app/controllers/arrivals_controller.rb
+++ app/controllers/arrivals_controller.rb
@@ -9,11 +9,42 @@ class ArrivalsController < ApplicationController
 
   def create
     @arrival = Arrival.create(user_params)
-    if @bus.valid?
+    if @arrival.valid?
       flash[:notice] = "New Arrival Added"
-      redirect_to arrival_path
+      redirect_to arrival_path(@arrival)
     else
       render 'new'
     end
+  end
+
+  def show
+    @arrival = Arrival.find(params[:id])
+  end
+
+  def edit
+    @arrival = Arrival.find(params[:id])
+  end
+
+  def update
+    @arrival = Arrival.find(params[:id])
+    if @arrival.update(user_params)
+      flash[:notice] = "Arrival updated"
+      redirect_to arrival_path(@arrival)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @arrival = Arrival.find(params[:id])
+    @arrival.destroy
+    flash[:notice] = "This bus will no longer arrive at #{@arrival.names} at #{@arrival.time}"
+    redirect_to arrivals_path
+  end
+
+
+  private
+  def user_params
+    params.require(:arrival).permit(:time, :stop_id, :bus_id)
   end
 end
--- app/models/arrival.rb
+++ app/models/arrival.rb
@@ -2,4 +2,8 @@ class Arrival < ActiveRecord::Base
   validates :time, presence: true
   belongs_to :stop
   belongs_to :bus
+
+  def names
+    "#{stop.station.name}" + " - " + "#{stop.line.name}"
+  end
 end
