6d7f9e6ca500350e7c8fa6e135c0890863658237
Meghan Lindsley & Tanner Steward <student@epicodus.com>
2014-09-03 12:08:13 -0700

    Add index, new, and create methods for bus model, along with views

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,3 +1,26 @@
 class BusesController < ApplicationController
+ def index
+    @buses = Bus.all
+  end
 
+  def new
+    @bus= Bus.new
+  end
+
+  def create
+    @bus = Bus.create(user_params)
+    if @bus.valid?
+      flash[:notice] = "New Bus Added"
+      redirect_to bus_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:bus).permit(:number, :line_id)
+  end
 end
+
+
