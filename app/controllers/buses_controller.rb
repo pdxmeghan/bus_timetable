f1d77052029758d122a1d5eb77bb6d3844c274bc
Meghan Lindsley & Tanner Steward <student@epicodus.com>
2014-09-03 13:43:24 -0700

    edit and delete for buses

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -21,6 +21,27 @@ class BusesController < ApplicationController
     @bus = Bus.find(params[:id])
   end
 
+  def edit
+    @bus = Bus.find(params[:id])
+  end
+
+  def update
+    @bus = Bus.find(params[:id])
+    if @bus.update(user_params)
+      flash[:notice] = "This bus has been updated"
+      redirect_to bus_path(@bus)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @bus = Bus.find(params[:id])
+    @bus.destroy
+    flash[:notice]= "Bus number #{@bus.number} has been deleted."
+    redirect_to buses_path
+  end
+
   private
   def user_params
     params.require(:bus).permit(:number, :line_id)
